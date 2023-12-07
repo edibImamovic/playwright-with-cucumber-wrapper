9e932edbc45d415355843322ad283c8f2d16c016
Edib Imamovic <edib.imamovic@gmail.com>
2023-12-07 13:00:16 +0100

    - github run update

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,7 @@
-// scripts/test.js
-
 const { execSync } = require('child_process');
+
+// Function to check if running on GitHub Actions
+const isGitHubActions = () => process.env.GITHUB_ACTIONS === 'true';
 
 try {
   // Run the test command
@@ -13,8 +14,13 @@ try {
   console.error('Test failed:', error);
 }
 
-// Generate the report
-const reportCommand = `npx monocart show-report './test-results/report.html'`;
-const options = { stdio: 'inherit' };
+// Only generate and open the report if not running on GitHub Actions
+if (!isGitHubActions()) {
+  const reportCommand = `npx monocart show-report './test-results/report.html'`;
+  const options = { stdio: 'inherit' };
 
-execSync(reportCommand, options);
+  execSync(reportCommand, options);
+
+  // Optional: Close the report after a set time (requires additional logic based on how the report is displayed)
+  // setTimeout(() => { /* Logic to close report */ }, 10000); // Closes after 10 seconds
+}
